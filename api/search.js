e90b515f426f5a2019560a6cacc3766a046fe5e7
StephenRed94 <stephen.rr94@gmail.com>
2024-11-27 15:39:39 -0500

    Testing

--- api/search.js
+++ api/search.js
@@ -8,9 +8,8 @@ const TMDB_TV_STREAMING_API_URL = 'https://api.themoviedb.org/3/watch/providers/
 
 module.exports = async (req, res) => {
   const query = req.query.query;  // Get the search query from the frontend
-  const allowedOrigins = ['http://127.0.0.1:5500/Streamly-Frontend/', 'http://127.0.0.1:5500/']; 
 
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/Streamly-Frontend/');
   res.setHeader('Access-Control-Allow-Methods', 'GET'); 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
 
